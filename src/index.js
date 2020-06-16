a0b974b1fd382aac8a594fcbeedf05d9fa5f6cf5
Kirill Zayats <kirill.zayats77@gmail.com>
2020-06-16 18:51:30 +0300

    feat: try connect style to html

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
-import _ from 'lodash';
 
-function component() {
-    const element = document.getElementsByClassName("body")
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+console.log("Hello Worl")
