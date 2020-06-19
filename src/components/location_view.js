79851c0eb4bcca8cff001ef92e9467979fd4fc63
Kirill Zayats <kirill.zayats77@gmail.com>
2020-06-19 09:45:09 +0300

    feat: add find search by city name

--- src/components/location_view.js
+++ src/components/location_view.js
@@ -1,15 +1,11 @@
 import controller from "./location_controller.js";
 import coordinates from "./coordinates_model.js";
 import load_map  from './maps_model.js';
+import {obj} from "../constants/geolocation.js"
 
 console.log("connection location_view 1")
 
-getlocation()
-
-function initiate(){
-  var get=document.getElementById('getlocation'); 
-  get.addEventListener('click', getlocation, false);
-}
+navigator.geolocation.getCurrentPosition(showinfo, error, options);
 
 var options = {
   enableHighAccuracy: true,
@@ -17,20 +13,54 @@ var options = {
   maximumAge: 0
 };
 
-function getlocation(){
-   navigator.geolocation.getCurrentPosition(showinfo, error, options);
-
-}
-
 function showinfo(position){ 
-  var latitude = position.coords.latitude.toFixed(4); 
-  var longitude = position.coords.longitude.toFixed(4);
-  document.getElementById('get_latitude').innerHTML = "Latitude " +  latitude;
-  document.getElementById("get_longitude").innerHTML = "Longitude " + longitude;
-  load_map(latitude, longitude)
+  console.log(obj.latitude);
+  obj.latitude = position.coords.latitude.toFixed(4); 
+  obj.longitude = position.coords.longitude.toFixed(4);
+  console.log("PR")
+  document.getElementById('get_latitude').innerHTML = "Latitude " +  obj.latitude;
+  document.getElementById("get_longitude").innerHTML = "Longitude " + obj.longitude;
+  load_map();
 }
 
 function error(err) {
   console.warn(`ERROR(${err.code}): ${err.message}`);
 };   
-window.addEventListener('load', initiate, false);
+
+// console.log("RRRRRRRRRRRRR")
+
+
+// // getlocation()
+
+// function initiate(){
+//   console.log("RRRRRRRRRRRRR")
+
+//   var get=document.getElementById('get_location_usind_address'); 
+//   get.addEventListener('click', load_map(), false);
+
+// }
+// window.addEventListener('', initiate, false);
+
+// var options = {
+//   enableHighAccuracy: true,
+//   timeout: 5000,
+//   maximumAge: 0
+// };
+
+// function getlocation(){
+//    navigator.geolocation.getCurrentPosition(showinfo, error, options);
+
+// }
+
+// function showinfo(position){ 
+//   var latitude = position.coords.latitude.toFixed(4); 
+//   var longitude = position.coords.longitude.toFixed(4);
+//   document.getElementById('get_latitude').innerHTML = "Latitude " +  latitude;
+//   document.getElementById("get_longitude").innerHTML = "Longitude " + longitude;
+//   load_map(latitude, longitude)
+// }
+
+// function error(err) {
+//   console.warn(`ERROR(${err.code}): ${err.message}`);
+// };   
+// window.addEventListener('load', initiate, false);
